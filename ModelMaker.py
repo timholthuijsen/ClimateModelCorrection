d377d59c938bb1fa712df2bcb192248ac2a4019d
timholthuijsen <timholthuijsen@gmail.com>
2021-05-11 14:21:08 +0200

    Adding disclaimer to ModelMaker.py

--- ModelMaker.py
+++ ModelMaker.py
@@ -2,4 +2,9 @@
 
 text = input("Please provide the path to your .nc climate model output file: ")
 
+"""
+Note: This program is still currently under development
+"""
+
+
 print("your nc file is:", text)
